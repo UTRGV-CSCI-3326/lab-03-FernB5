9c21c533e59e184c9e256f6bc0fbf6350b5429f0
FernB5 <fernando.beltranhernandez01@utrgv.edu>
2025-02-02 19:55:11 +0000

    First submission

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,18 @@
+public class BankAccount {
+    public static void main(String[] args) {
 
+        // Starting balance
+        float bobAccount = 2175.37f;
+
+        // Performed transactions
+        bobAccount = bobAccount - 302.50f;
+        bobAccount = bobAccount + 50.03f;
+        bobAccount = (bobAccount / 2) + 20;
+        bobAccount = bobAccount - 1;
+        bobAccount = bobAccount * 2;
+        bobAccount = bobAccount + 1;
+
+        // Bob's new balance
+        System.out.printf("%.2f",bobAccount);
+    }
+}
